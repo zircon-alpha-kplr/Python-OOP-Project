012edb5cf7a798e75d11a9c37806f7ac3a554711
zircon-alpha-kplr <124888869+zircon-alpha-kplr@users.noreply.github.com>
2023-03-07 11:10:06 +0000

    VDH

--- exercices/03.class_tree/01.class_tree_exe.py
+++ exercices/03.class_tree/01.class_tree_exe.py
@@ -14,25 +14,39 @@ def json_dict_from_file():
     return json_dict
 
 # func to create a tree from a dict
-def create_tree_from_dict(tree, parent_node_id, parent_dict):
-    for key, value in parent_dict.items():
-#        if key != ("subclasses"):
-            if isinstance(value, dict):
-               # Créer un nouveau noeud pour la clé courante du dictionnaire
-               new_node_id = f"{parent_node_id}.{key}"
-               tree.create_node(tag=key, identifier=new_node_id, parent=parent_node_id)
-               create_tree_from_dict(tree, new_node_id, value) # Créer récursivement le sous-arbre pour le dictionnaire courant
- #       else:
- #           continue
+def create_tree_from_dict(json_dict):
+    # define the tree
+    global tree 
+    tree = Tree()
 
-# func to create recursive tree
+    # define the root node
+    root_node_id = "root"
+    root_node_name = "Product Classes Hierarchy"
+    tree.create_node(root_node_name, root_node_id)
+
+    #traverse json data and creathe other node
+    recursive_tree_from_dict(json_dict, root_node_id)
+
+    return tree
+
+
+def recursive_tree_from_dict(json_dict, parent_node_id):
+    for class_name, class_attrs in json_dict.items():
+            class_node_id = class_name
+            class_node_name = class_name
+
+            # Add the class node to the tree
+            tree.create_node(class_node_name, class_node_id, parent=parent_node_id)
+
+            # Traverse any subclasses
+            if "subclasses" in class_attrs:
+                recursive_tree_from_dict(class_attrs["subclasses"], class_node_id)
+
 
 # func MAIN
 def main():
     json_dict = json_dict_from_file() #call func "json_dict_from_file() : Load JSON and convert to dict
-    my_tree = Tree() #create Tree
-    my_tree.create_node(tag="Product Classes Hierarchy", identifier="racine") #create le noeud racine pour l'arbre
-    create_tree_from_dict(my_tree, "racine", json_dict) #créer la structure de l'arbre à partir du dictionnaire
+    my_tree = create_tree_from_dict(json_dict)
     my_tree.show() #Show class tree
 
 
