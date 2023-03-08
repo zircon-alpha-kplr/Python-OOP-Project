64da4d6a1416019da9bef1c345ca7f7977924e3c
zircon-alpha-kplr <124888869+zircon-alpha-kplr@users.noreply.github.com>
2023-03-08 09:43:50 +0000

    VDH

--- exercices/04.class_generation/02.class_generation_vdh.py
+++ exercices/04.class_generation/02.class_generation_vdh.py
@@ -1,4 +1,4 @@
-def generate_class_def(nom_classe: str, attributs: dict, nom_superclasse: str, args_superclasse: list = [])
+def generate_class_def(nom_classe: str, attributs: dict, nom_superclasse: str, args_superclasse: list = []):
     args_constructeur = []
     definition_constructeur = ""
     has_attributs = False
@@ -12,7 +12,7 @@ def generate_class_def(nom_classe: str, attributs: dict, nom_superclasse: str, a
 
     # Gestion des attributs
     for nom_attribut in attributs.keys():
-        if nom_attribut != "sublclasses":
+        if nom_attribut != "subclasses":
             has_attributs = True
             args_constructeur.append(nom_attribut)
             definition_constructeur += f"\n\t self.{nom_attribut} = {nom_attribut}"
@@ -23,21 +23,27 @@ def generate_class_def(nom_classe: str, attributs: dict, nom_superclasse: str, a
 
     # Gestion constructeur
     if has_attributs:
-        modele_constructeur = f"\t def __init__(self, {", ".join(args_constructeur + args_superclasse)}) : "
+        modele_constructeur = f"\t def __init__(self, {', '.join(args_constructeur + args_superclasse)}) : "
 
         if len(args_superclasse) > 0:
-            modele_constructeur += definition_constructeur
+            modele_constructeur += f"\n\t\t super().__init__({', '.join(args_superclasse)})"
+        
+        modele_constructeur += definition_constructeur
 
     else:
         if len(args_superclasse) > 0:
-            modele_constructeur = f"\t def __init__(self, {", ".join(args_superclasse)}) : "
-            modele_constructeur = f"\n\t\t super().__init__({", ".join(args_superclasse)})"
+            modele_constructeur = f"\t def __init__(self, {', '.join(args_superclasse)}) : "
+            modele_constructeur = f"\n\t\t super().__init__({', '.join(args_superclasse)})"
     
         else:
             modele_constructeur = "\t pass"
 
     return modele_classe + modele_constructeur + "\n\n"
 
+
+if __name__ == '__main__':
+    # Appeler la fonction principale
+    test_fonction()
 
 
 def test_fonction():
