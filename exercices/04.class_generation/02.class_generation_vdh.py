069346af85455e32aa4d2b447d7c3d467a3b1b54
zircon-alpha-kplr <124888869+zircon-alpha-kplr@users.noreply.github.com>
2023-03-08 08:51:07 +0000

    VDH

--- exercices/04.class_generation/02.class_generation_vdh.py
+++ exercices/04.class_generation/02.class_generation_vdh.py
@@ -4,22 +4,47 @@ def generate_class_def(nom_classe: str, attributs: dict, nom_superclasse: str, a
     has_attributs = False
     modele_classe = f"class {nom_classe}"
 
-# Gestion de la superclasse
-if nom_superclasse:
-    modele_classe += " :\n"
+    # Gestion de la superclasse
+    if nom_superclasse:
+        modele_classe += f"({nom_superclasse})"
+    
+        modele_classe += " :\n"
 
-# Gestion de la classe
-if nom_classe == "Product":
-    definition_constructeur += "\n\t\t self.name = type(self).__name__"
+    # Gestion des attributs
+    for nom_attribut in attributs.keys():
+        if nom_attribut != "sublclasses":
+            has_attributs = True
+            args_constructeur.append(nom_attribut)
+            definition_constructeur += f"\n\t self.{nom_attribut} = {nom_attribut}"
 
-# Gestion constructeur
-if has_attributs:
-    modele_constructeur = f"\t def __init__(self, {", ".join(args_constructeur + args_superclasse)}) : "
+    # Gestion de la classe
+    if nom_classe == "Product":
+        definition_constructeur += "\n\t\t self.name = type(self).__name__"
 
-    if len(args_superclasse) > 0:
-        modele_constructeur += definition_constructeur
+    # Gestion constructeur
+    if has_attributs:
+        modele_constructeur = f"\t def __init__(self, {", ".join(args_constructeur + args_superclasse)}) : "
 
-else:
-    if len(args_superclasse) > 0:
-        modele_constructeur = f"\t def __init__(self, {", ".join(args_superclasse)}) : "
-        modele_constructeur = f"\n\t\t super().__init__({", ".join(args_superclasse)})"
+        if len(args_superclasse) > 0:
+            modele_constructeur += definition_constructeur
+
+    else:
+        if len(args_superclasse) > 0:
+            modele_constructeur = f"\t def __init__(self, {", ".join(args_superclasse)}) : "
+            modele_constructeur = f"\n\t\t super().__init__({", ".join(args_superclasse)})"
+    
+        else:
+            modele_constructeur = "\t pass"
+
+    return modele_classe + modele_constructeur + "\n\n"
+
+
+
+def test_fonction():
+    attributs = {
+    "moteur": "str",
+    "nbportes": "int"
+    }
+
+    code_classe = generate_class_def("Voiture", attributs, "Vehicule", ["marque", "modele"])
+    print(code_classe)
